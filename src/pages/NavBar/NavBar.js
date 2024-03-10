15035a0ea32185673df5f18742bab6cff3fa00c9
thirupathi1431 <thirupathiraouppu1@gmail.com>
2024-03-10 20:34:42 +0530

    sidemenubar

--- src/pages/NavBar/NavBar.js
+++ src/pages/NavBar/NavBar.js
@@ -3,13 +3,33 @@ import React from 'react';
 import "./NavBar.css";
 import Search from '../../components/Search/Search';
 import NavDetails from '../../components/NavDetails/NavDetails';
-// import Logo from '../Logo'
+import Logo from '../../images/mahalogo.png'
 
 const NavBar = () => {
+
+  const handleToggleSideBar = () => {
+    document.body.classList.toggle('toggle-sidebar');
+  };
+
+
   return (
     <header id="header" className="header fixed-top d-flex align-items-center">
       {/* <Logo /> */}
-      <h4>MahaBank</h4>
+      <div className="d-flex align-items-center justify-content-between">
+        
+      <a  className="logo d-flex align-items-center" onClick={handleToggleSideBar}>
+      
+        <img src={Logo} alt=""/>
+        <span className="d-none d-lg-block">MAHA CO-OPERATIVE SOCIETY</span>
+      </a>
+     
+
+      <i
+        className=" d-none d-lg-block bi bi-list toggle-sidebar-btn "
+        onClick={handleToggleSideBar}
+      ></i>
+    </div>
+     
       <Search />
       <NavDetails />
     </header>
